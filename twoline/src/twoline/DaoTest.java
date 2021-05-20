5fdb509b7bb136e0344b30d86f75076bf79ea359
YuDongJae <ydjdj2@naver.com>
2021-05-20 09:20:08 +0900

    twoline

--- twoline/src/twoline/DaoTest.java
+++ twoline/src/twoline/DaoTest.java
@@ -15,12 +15,14 @@ public class DaoTest {
 		// 1. 데이터를 널어보고
 		dto.setMemo("TestTest");
 		dao.insert(dto);;
+		
 		// 2. 그 데이터를 출력해보고
 		ArrayList<OnelineDTO> dtos = dao.getList();
 		for(int i = 0 ; i < dtos.size(); i++) {
 			OnelineDTO dt = dtos.get(i);
 			System.out.println("no : " + dt.getNo() + ", memo: " + dt.getMemo() + ", date: " +dt.getWdate());
 		}
+		
 		// 3. 그 데이터를 삭제하는 것 까지 
 
 	}
